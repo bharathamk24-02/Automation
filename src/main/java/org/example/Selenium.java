89336ff7dac651947eb9cd88be1d4e6844eaa7f5
Bharath Kumar Muthukumar <bharathkumar.muthukumar@expleogroup.com>
2026-02-10 23:18:29 +0530

    first commit api

--- src/main/java/org/example/Selenium.java
+++ src/main/java/org/example/Selenium.java
@@ -86,104 +86,104 @@ public class Selenium {
 
         //Guideware Account Creation
 
-        try {
-
-            String userNameAndPassword = "exp_test";
-            WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("http://10.192.190.131:8180/pc/PolicyCenter.do");
-            Thread.sleep(1000);
-            WebElement userName = driver.findElement(By.xpath("//input[@id=\"Login:LoginScreen:LoginDV:username-inputEl\"]"));
-            userName.sendKeys(userNameAndPassword);
-            Thread.sleep(2000);
-            driver.findElement(By.xpath("//input[@id=\"Login:LoginScreen:LoginDV:password-inputEl\"]")).sendKeys(userNameAndPassword);
-            Thread.sleep(2000);
-            driver.findElement(By.xpath("//span[@id=\"Login:LoginScreen:LoginDV:submit-btnInnerEl\"]")).click();
-            Thread.sleep(1000);
-
-            Actions actions = new Actions(driver);
-            WebElement dropdown = driver.findElement(
-                    By.xpath("//span[contains(@id,'AccountTab-btnEl')]")
-            );
-            actions.moveToElement(dropdown,30,0).click().perform();
-            Thread.sleep(2000);
-
-            driver.findElement(By.xpath("//span[@id=\"TabBar:AccountTab:AccountTab_NewAccount-textEl\"]")).click();
-
-            Thread.sleep(1000);
-            WebElement enterFirstName = driver.findElement(By.xpath("//input[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:GlobalPersonNameInputSet:FirstName-inputEl\"]"));
-            enterFirstName.sendKeys("Bharath");
-
-            WebElement enterLastname = driver.findElement(By.xpath("//input[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:GlobalPersonNameInputSet:LastName-inputEl\"]"));
-            enterLastname.sendKeys("kumar");
-
+//        try {
 //
-
-            WebElement clickSubmitbtn = driver.findElement(By.xpath("//a[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:SearchAndResetInputSet:SearchLinksInputSet:Search\"]"));
-            clickSubmitbtn.click();
-
-            Thread.sleep(1000);
-            driver.findElement(By.id("NewAccount:NewAccountScreen:NewAccountButton-btnInnerEl")).click();
-
-            Thread.sleep(2000);
-            driver.findElement(By.xpath("//a[@id=\"NewAccount:NewAccountScreen:NewAccountButton:NewAccount_Person-itemEl\"]")).click();
-
-            Thread.sleep(2000);
-
-            WebElement enterAddress = driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:AddressLine1-inputEl"));
-            String address = RandomStringUtils.randomAlphabetic(3);
-            System.out.println(address);
-            enterAddress.sendKeys("Test-"+ address);;
-
-            WebElement pincode = driver.findElement(By.xpath("//input[@id=\"CreateAccount:CreateAccountScreen:CreateAccountDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:PostalCode-inputEl\"]"));
-            pincode.sendKeys("99501");
-            Thread.sleep(3000);
-              driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressDescription-inputEl")).click();
-
-              driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressType-inputEl")).click();
-//            Actions actions1 = new Actions(driver);
-//            actions1.contextClick(addressType);
-            Thread.sleep(1000);
-            System.out.println("Printed_sample-1");
-            driver.findElement(By.xpath("(//li[contains(text(),'Billing')])")).click();
-            Thread.sleep(1000);
-
-            driver.findElement(By.xpath("//div[@id=\"CreateAccount:CreateAccountScreen:CreateAccountDV:ProducerSelectionInputSet:Producer:SelectOrganization\"]")).click();
-            Thread.sleep(1000);
-            driver.findElement(By.xpath("//input[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchDV:GlobalContactNameInputSet:Name-inputEl\"]")).sendKeys("a");
-            driver.findElement(By.xpath("//a[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchDV:SearchAndResetInputSet:SearchLinksInputSet:Search\"]")).click();
-            Thread.sleep(1000);
-            driver.findElement(By.xpath("//a[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchResultsLV:0:_Select\"]")).click();
-            Thread.sleep(1000);
-            driver.findElement(By.xpath("//span[@id=\"CreateAccount:CreateAccountScreen:Update-btnInnerEl\"]")).click();
-            Thread.sleep(5000);
-
-            WebElement expectedAccNum =driver.findElement(By.xpath("//div[@id=\"AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:AccountNumber-bodyEl\"]"));
-            System.out.println("AccountNum :"+ expectedAccNum.getText());
-
-            Thread.sleep(1000);
-            List<WebElement> expectedAccName =driver.findElements(By.xpath("//div[@id='AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:Name-inputEl']"));
-            for (int i =0 ;i<expectedAccName.size();i++) {
-                System.out.println("Address :" + expectedAccName.get(i).getText());
-            }
-
-
-
-
-
-            Thread.sleep(1000);
-
-            List<WebElement> expectedAddress =driver.findElements(By.xpath("//div[@id=\"AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:AddressShortInputSet:globalAddressContainer:GlobalAddressInputSet:AddressSummary-inputEl\"]"));
-            for (int i =0 ;i<expectedAddress.size();i++){
-                System.out.println("Address :"+ expectedAddress.get(i).getText());
-            }
-
-
-            driver.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-
-        }
+//            String userNameAndPassword = "exp_test";
+//            WebDriver driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+//            driver.get("http://10.192.190.131:8180/pc/PolicyCenter.do");
+//            Thread.sleep(1000);
+//            WebElement userName = driver.findElement(By.xpath("//input[@id=\"Login:LoginScreen:LoginDV:username-inputEl\"]"));
+//            userName.sendKeys(userNameAndPassword);
+//            Thread.sleep(2000);
+//            driver.findElement(By.xpath("//input[@id=\"Login:LoginScreen:LoginDV:password-inputEl\"]")).sendKeys(userNameAndPassword);
+//            Thread.sleep(2000);
+//            driver.findElement(By.xpath("//span[@id=\"Login:LoginScreen:LoginDV:submit-btnInnerEl\"]")).click();
+//            Thread.sleep(1000);
+//
+//            Actions actions = new Actions(driver);
+//            WebElement dropdown = driver.findElement(
+//                    By.xpath("//span[contains(@id,'AccountTab-btnEl')]")
+//            );
+//            actions.moveToElement(dropdown,30,0).click().perform();
+//            Thread.sleep(2000);
+//
+//            driver.findElement(By.xpath("//span[@id=\"TabBar:AccountTab:AccountTab_NewAccount-textEl\"]")).click();
+//
+//            Thread.sleep(1000);
+//            WebElement enterFirstName = driver.findElement(By.xpath("//input[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:GlobalPersonNameInputSet:FirstName-inputEl\"]"));
+//            enterFirstName.sendKeys("Bharath");
+//
+//            WebElement enterLastname = driver.findElement(By.xpath("//input[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:GlobalPersonNameInputSet:LastName-inputEl\"]"));
+//            enterLastname.sendKeys("kumar");
+//
+////
+//
+//            WebElement clickSubmitbtn = driver.findElement(By.xpath("//a[@id=\"NewAccount:NewAccountScreen:NewAccountSearchDV:SearchAndResetInputSet:SearchLinksInputSet:Search\"]"));
+//            clickSubmitbtn.click();
+//
+//            Thread.sleep(1000);
+//            driver.findElement(By.id("NewAccount:NewAccountScreen:NewAccountButton-btnInnerEl")).click();
+//
+//            Thread.sleep(2000);
+//            driver.findElement(By.xpath("//a[@id=\"NewAccount:NewAccountScreen:NewAccountButton:NewAccount_Person-itemEl\"]")).click();
+//
+//            Thread.sleep(2000);
+//
+//            WebElement enterAddress = driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:AddressLine1-inputEl"));
+//            String address = RandomStringUtils.randomAlphabetic(3);
+//            System.out.println(address);
+//            enterAddress.sendKeys("Test-"+ address);;
+//
+//            WebElement pincode = driver.findElement(By.xpath("//input[@id=\"CreateAccount:CreateAccountScreen:CreateAccountDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:PostalCode-inputEl\"]"));
+//            pincode.sendKeys("99501");
+//            Thread.sleep(3000);
+//              driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressDescription-inputEl")).click();
+//
+//              driver.findElement(By.id("CreateAccount:CreateAccountScreen:CreateAccountDV:AddressType-inputEl")).click();
+////            Actions actions1 = new Actions(driver);
+////            actions1.contextClick(addressType);
+//            Thread.sleep(1000);
+//            System.out.println("Printed_sample-1");
+//            driver.findElement(By.xpath("(//li[contains(text(),'Billing')])")).click();
+//            Thread.sleep(1000);
+//
+//            driver.findElement(By.xpath("//div[@id=\"CreateAccount:CreateAccountScreen:CreateAccountDV:ProducerSelectionInputSet:Producer:SelectOrganization\"]")).click();
+//            Thread.sleep(1000);
+//            driver.findElement(By.xpath("//input[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchDV:GlobalContactNameInputSet:Name-inputEl\"]")).sendKeys("a");
+//            driver.findElement(By.xpath("//a[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchDV:SearchAndResetInputSet:SearchLinksInputSet:Search\"]")).click();
+//            Thread.sleep(1000);
+//            driver.findElement(By.xpath("//a[@id=\"OrganizationSearchPopup:OrganizationSearchPopupScreen:OrganizationSearchResultsLV:0:_Select\"]")).click();
+//            Thread.sleep(1000);
+//            driver.findElement(By.xpath("//span[@id=\"CreateAccount:CreateAccountScreen:Update-btnInnerEl\"]")).click();
+//            Thread.sleep(5000);
+//
+//            WebElement expectedAccNum =driver.findElement(By.xpath("//div[@id=\"AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:AccountNumber-bodyEl\"]"));
+//            System.out.println("AccountNum :"+ expectedAccNum.getText());
+//
+//            Thread.sleep(1000);
+//            List<WebElement> expectedAccName =driver.findElements(By.xpath("//div[@id='AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:Name-inputEl']"));
+//            for (int i =0 ;i<expectedAccName.size();i++) {
+//                System.out.println("Address :" + expectedAccName.get(i).getText());
+//            }
+//
+//
+//
+//
+//
+//            Thread.sleep(1000);
+//
+//            List<WebElement> expectedAddress =driver.findElements(By.xpath("//div[@id=\"AccountFile_Summary:AccountFile_SummaryScreen:AccountFile_Summary_BasicInfoDV:AddressShortInputSet:globalAddressContainer:GlobalAddressInputSet:AddressSummary-inputEl\"]"));
+//            for (int i =0 ;i<expectedAddress.size();i++){
+//                System.out.println("Address :"+ expectedAddress.get(i).getText());
+//            }
+//
+//
+//            driver.close();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//
+//        }
 
 
     }}
